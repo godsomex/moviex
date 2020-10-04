fadf49b5503a4082aa927749b1736e96a1638003
Godspower <goc.omenihu@gmail.com>
2020-10-04 13:46:31 +0200

    init: init commit with emotion css

---
+++ App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled, {css} from '@emotion/native';
+import {SafeAreaView, ScrollView, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <SafeAreaView>
+        <Scroll contentInsetAdjustmentBehavior="automatic">
+          <Container>
+            <Title> Moviex App</Title>
+          </Container>
+        </Scroll>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
+
+const Container = styled.View`
+  flex: 1;
+  background-color: grey;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  font-size: 20px;
+  font-weight: 500;
+  color: blue;
+`;
+
+const Scroll = styled.ScrollView`
+  backgroundcolor: Colors.lighter;
+`;
