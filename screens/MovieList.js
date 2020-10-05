abe1c872db6b847c89e07f821629bdbfb843d50f
Godspower <goc.omenihu@gmail.com>
2020-10-05 09:14:28 +0200

    screens: movieList layout with mocked data

--- screens/MovieList.js
+++ screens/MovieList.js
@@ -1,10 +1,153 @@
-import React from 'react';
-import {SafeAreaView, ScrollView, View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  Image,
+  Alert,
+  ScrollView,
+  TextInput,
+  FlatList,
+} from 'react-native';
+
+import {movieList} from '../utilities/index';
 
 function MovieList(props) {
-  const {title} = props.route.params;
+  const [state] = useState(movieList);
+  const [searchterm, setSearchterm] = useState('');
+  console.log(props.route.params.id);
+  const genreId = props.route.params.id;
 
-  return <Text>{title}</Text>;
+  const clickEventListener = (term) => {
+    Alert.alert('Alert', 'Button pressed ' + genreId + term);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.formContent}>
+        <View style={styles.inputContainer}>
+          <Image
+            style={[styles.icon, styles.inputIcon]}
+            source={{
+              uri: 'https://img.icons8.com/search/androidL/100/2ecc71',
+            }}
+          />
+          <TextInput
+            style={styles.inputs}
+            placeholder="Search"
+            underlineColorAndroid="transparent"
+            onChangeText={(text) => setSearchterm(text)}
+            value={searchterm}
+          />
+        </View>
+
+        <TouchableHighlight
+          style={styles.saveButton}
+          onPress={() => clickEventListener(searchterm)}>
+          <Image
+            style={[styles.icon, styles.iconBtnSearch]}
+            source={{
+              uri: 'https://img.icons8.com/search/androidL/100/ffffff',
+            }}
+          />
+        </TouchableHighlight>
+      </View>
+
+      <FlatList
+        style={styles.notificationList}
+        enableEmptySections={true}
+        data={state.results}
+        renderItem={({item}) => {
+          return (
+            <View style={styles.notificationBox}>
+              <Image
+                style={styles.image}
+                source={{
+                  uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
+                }}
+              />
+
+              <Text style={styles.description}>{item.title}</Text>
+            </View>
+          );
+        }}
+      />
+    </View>
+  );
 }
 
 export default MovieList;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EBEBEB',
+  },
+  formContent: {
+    flexDirection: 'row',
+    marginTop: 30,
+  },
+  inputContainer: {
+    borderBottomColor: '#F5FCFF',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 30,
+    borderBottomWidth: 1,
+    height: 45,
+    flexDirection: 'row',
+    alignItems: 'center',
+    flex: 1,
+    margin: 10,
+  },
+  icon: {
+    width: 30,
+    height: 30,
+  },
+  iconBtnSearch: {
+    alignSelf: 'center',
+  },
+  inputs: {
+    height: 45,
+    marginLeft: 16,
+    borderBottomColor: '#FFFFFF',
+    flex: 1,
+  },
+  inputIcon: {
+    marginLeft: 15,
+    justifyContent: 'center',
+  },
+  saveButton: {
+    height: 45,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+    width: 70,
+    alignSelf: 'flex-end',
+    backgroundColor: '#40E0D0',
+    borderRadius: 30,
+  },
+  saveButtonText: {
+    color: 'white',
+  },
+  notificationList: {
+    marginTop: 20,
+    padding: 10,
+  },
+  notificationBox: {
+    padding: 20,
+    marginTop: 5,
+    marginBottom: 5,
+    backgroundColor: '#FFFFFF',
+    flexDirection: 'row',
+    borderRadius: 10,
+  },
+  image: {
+    width: 45,
+    height: 45,
+  },
+  description: {
+    fontSize: 18,
+    color: '#3498db',
+    marginLeft: 10,
+  },
+});
